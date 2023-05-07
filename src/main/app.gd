916f6e90739ef45bfcb64f987523609b424e38eb
unknown <rogertenz@gmail.com>
2023-05-06 21:22:52 -0700

    restructure

--- src/main/app.gd
+++ src/main/app.gd
@@ -1,16 +1,21 @@
-extends Node2D
+extends Spatial
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
+var main_gui = Control.new()
+var fps_counter = Label.new()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	main_gui.parent = self
+	fps_counter.name = "FPSCounter"
+	fps_counter.text = "FPS: "
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	fps_counter.text = "FPS: " + str(1 / delta)
