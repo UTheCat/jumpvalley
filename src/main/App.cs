f521cf4030eb5d7a1e862263e6fdc5ee74d90491
unknown <rogertenz@gmail.com>
2023-05-21 01:19:55 -0700

    Update App.cs

--- src/main/App.cs
+++ src/main/App.cs
@@ -1,14 +1,20 @@
 using Godot;
 //using System;
 
+// refer to this article for naming conventions:
+// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions
 public partial class App : Node
 {
 	private Control mainGui = new Control();
 	private Label fpsCounter = new Label();
 
+	private BoxSpinner spinner;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		spinner = new BoxSpinner((CsgBox3D) GetNode("Map/CSGBox"), 1);
+
 		fpsCounter.Name = "FPSCounter";
 		fpsCounter.Text = "FPS: ";
 		mainGui.AddChild(fpsCounter);
@@ -19,5 +25,6 @@ public partial class App : Node
 	public override void _Process(double delta)
 	{
 		fpsCounter.Text = "FPS: " + (1 / delta);
+		spinner.RotateInFrame(delta);
 	}
 }
