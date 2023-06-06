6ee72fa1460d9272d4fa8ab686ef4c6cb42f0908
udev <bees9633@gmail.com>
2023-06-06 16:26:30 -0700

    finished Playlist.cs for now
    hopefully it works

--- src/main/Music/Playlist.cs
+++ src/main/Music/Playlist.cs
@@ -7,13 +7,27 @@ using Godot;
 public partial class Playlist
 {
     /// <summary>
+    /// Converts a volume percentage in the range of [0, 1] to the corresponding value in decibels and returns the result
+    /// </summary>
+    /// <returns></returns>
+    public static float VolPercentToDecibels(float percent)
+    {
+        return Mathf.LinearToDb(percent);
+    }
+
+    /// <summary>
     /// The number of seconds that the volume transitioning lasts when uninterrupted
     /// </summary>
     public double TransitionTime = 0;
 
     private List<Song> list = new List<Song>();
-    private AudioStreamPlayer musicPlayer = null;
+    private AudioStreamPlayer musicPlayer;
+    private Tween currentTween;
 
+    /// <summary>
+    /// Adds a song to the playlist
+    /// </summary>
+    /// <param name="s">The song to add</param>
     public void Add(Song s)
     {
         if (!list.Contains(s))
@@ -22,18 +36,110 @@ public partial class Playlist
         }
     }
 
+    /// <summary>
+    /// Removes a song from the playlist
+    /// </summary>
+    /// <param name="s">The song to remove</param>
     public void Remove(Song s)
     {
         list.Remove(s);
     }
 
+    private void createAudioStream()
+    {
+        if (musicPlayer == null)
+        {
+            musicPlayer = new AudioStreamPlayer();
+        }
+    }
+
+    private void switchToSong(Song s)
+    {
+        createAudioStream();
+        //AudioStream stream;
+        string path = s.FilePath;
+
+        // make the playlist loop the same song over again if only one song is present
+        bool canLoop = list.Count == 1;
+
+        if (path.EndsWith(".wav"))
+        {
+            AudioStreamWav stream = new AudioStreamWav();
+
+            if (canLoop)
+            {
+                stream.LoopMode = AudioStreamWav.LoopModeEnum.Forward;
+            }
+            else
+            {
+                stream.LoopMode = AudioStreamWav.LoopModeEnum.Disabled;
+            }
+
+            musicPlayer.Stream = stream;
+        }
+        else if (path.EndsWith(".ogg"))
+        {
+            AudioStreamOggVorbis stream = new AudioStreamOggVorbis();
+            stream.Loop = canLoop;
+            musicPlayer.Stream = stream;
+        }
+        else if (path.EndsWith(".mp3"))
+        {
+            AudioStreamMP3 stream = new AudioStreamMP3();
+            stream.Loop = canLoop;
+            musicPlayer.Stream = stream;
+        }
+        else
+        {
+            throw new System.Exception("The format of the song file is invalid. The file extension must be .wav, .ogg, or .mp3.");
+        }
+    }
+
+    private void killCurrentTween()
+    {
+        if (currentTween != null)
+        {
+            currentTween.Kill();
+            currentTween = null;
+        }
+    }
+
     public void Play()
     {
-
+        createAudioStream();
+        switchToSong(list[0]);
+        killCurrentTween();
+        currentTween = musicPlayer.CreateTween();
+        currentTween.TweenProperty(
+            musicPlayer,
+            "volume_db",
+            VolPercentToDecibels(1),
+            TransitionTime
+        );
+        currentTween.Finished += () =>
+        {
+            currentTween = null;
+        };
     }
 
     public void Stop()
     {
-
+        killCurrentTween();
+        currentTween = musicPlayer.CreateTween();
+        currentTween.TweenProperty(
+            musicPlayer,
+            "volume_db",
+            VolPercentToDecibels(0),
+            TransitionTime
+        );
+        currentTween.Finished += () =>
+        {
+            currentTween = null;
+            musicPlayer.Stop();
+            musicPlayer.Stream.Dispose();
+            musicPlayer.Stream = null;
+            musicPlayer.Dispose();
+            musicPlayer = null;
+        };
     }
 }
