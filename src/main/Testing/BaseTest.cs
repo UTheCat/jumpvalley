6d5e07cc6d8358c5460b5636979610cbe5b2b225
udev <83146800+UTheDev@users.noreply.github.com>
2023-06-18 19:43:44 -0700

    create BaseTest

---
+++ src/main/Testing/BaseTest.cs
@@ -0,0 +1,16 @@
+﻿namespace Jumpvalley.Testing
+{
+    /// <summary>
+    /// Base class for testing Jumpvalley's code
+    /// </summary>
+    public partial class BaseTest
+    {
+        /// <summary>
+        /// Testing class start method
+        /// </summary>
+        public virtual void Start()
+        {
+
+        }
+    }
+}
---
+++ src/main/Testing/SongPackageTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jumpvalley.Testing
+{
+    /// <summary>
+    /// Tests for loading a song via song package
+    /// </summary>
+    public partial class SongPackageTest: BaseTest
+    {
+
+    }
+}
