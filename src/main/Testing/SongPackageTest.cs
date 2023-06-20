74d15692d2119be7c53ffb3fa05be70cbd69a550
udev <83146800+UTheDev@users.noreply.github.com>
2023-06-19 18:34:05 -0700

    Update SongPackageTest.cs

--- src/main/Testing/SongPackageTest.cs
+++ src/main/Testing/SongPackageTest.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Godot;
+using System;
+
+using Jumpvalley.Music;
 
 namespace Jumpvalley.Testing
 {
@@ -7,6 +10,24 @@ namespace Jumpvalley.Testing
     /// </summary>
     public partial class SongPackageTest: BaseTest
     {
+        private string[] dirList;
+        private SceneTree tree;
 
+        public MusicPlayer MusicPlayer { get; private set; }
+
+        /// <summary>
+        /// Primary constructor
+        /// </summary>
+        /// <param name="sceneTree">The Godot scene tree to play music in</param>
+        /// <param name="directoryList">The list of directories to play</param>
+        public SongPackageTest(SceneTree sceneTree, string[] directoryList) {
+            dirList = directoryList;
+            tree = sceneTree;
+        }
+
+        public override void Start()
+        {
+
+        }
     }
 }
