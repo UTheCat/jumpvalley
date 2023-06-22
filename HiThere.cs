e722f0f62424243fba5676a59e0eba05f4371298
udev <83146800+UTheDev@users.noreply.github.com>
2023-06-21 17:40:28 -0700

    update

---
+++ HiThere.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Jumpvalley
+{
+    public partial class HiThere
+    {
+        public HiThere()
+        {
+            Console.WriteLine("hi :3");
+            Console.WriteLine("meow");
+        }
+    }
+}
