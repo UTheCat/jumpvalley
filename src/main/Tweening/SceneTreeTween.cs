6364ac42acc0c95bcabc23f6ec1a140ddc8442a5
udev <83146800+UTheDev@users.noreply.github.com>
2023-06-22 18:04:59 -0700

    Update SceneTreeTween.cs

--- src/main/Tweening/SceneTreeTween.cs
+++ src/main/Tweening/SceneTreeTween.cs
@@ -30,7 +30,7 @@ namespace Jumpvalley.Tweening
                 _tree = value;
 
                 // In case the tween is still playing while the value of "Tree" has changed, bind to the process step of the newly set SceneTree instance
-                if (value != null)
+                if (IsPlaying && value != null)
                 {
                     ConnectProcessStep();
                 }
