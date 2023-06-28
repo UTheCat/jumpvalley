ba56f1a53e8aa2a3176f04a2d42c32e3c0f73b0a
udev <83146800+UTheDev@users.noreply.github.com>
2023-06-27 19:46:27 -0700

    Update BaseMover.cs
    This should make it easier to work with the numbers regarding which way to travel.

--- src/main/Players/Movement/BaseMover.cs
+++ src/main/Players/Movement/BaseMover.cs
@@ -13,34 +13,38 @@ namespace Jumpvalley.Players.Movement
     public partial class BaseMover
     {
         /// <summary>
-        /// Magnitude in which the character wishes to go forward in the range of [0, 1].
+        /// Scalar in which the character wishes to go forward in the range of [-1, 1].
+        /// <br/>
+        /// A value less than 0 indicates that the character wants to go backwards while a value greater than 0 indicates that the character wants to go forwards.
         /// </summary>
         public float ForwardValue = 0;
 
         /// <summary>
         /// Magnitude in which the character wishes to go backward in the range of [0, 1].
         /// </summary>
-        public float BackwardValue = 0;
+        //public float BackwardValue = 0;
 
         /// <summary>
         /// Magnitude in which the character wishes to go left in the range of [0, 1].
         /// </summary>
-        public float LeftValue = 0;
+        //public float LeftValue = 0;
 
         /// <summary>
-        /// Magnitude in which the character wishes to go right in the range of [0, 1].
+        /// Scalar in which the character wishes to go right in the range of [-1, 1].
+        /// <br/>
+        /// A value less than 0 indicates that the character wants to go left while a value greater than 0 indicates that the character wants to go right.
         /// </summary>
         public float RightValue = 0;
 
         /// <summary>
         /// The gravity that the character experiences in meters per second squared.
         /// </summary>
-        public double Gravity = 9.8f;
+        public float Gravity = 9.8f;
 
         /// <summary>
         /// The initial velocity of the character's jump
         /// </summary>
-        public double JumpVelocity = 0;
+        public float JumpVelocity = 5f;
 
         /// <summary>
         /// Whether or not the player is currently climbing
@@ -53,8 +57,8 @@ namespace Jumpvalley.Players.Movement
         public bool IsJumping = false;
 
         /// <summary>
-        /// Calculates and returns the move vector for the current forward, backward, left, and right values
-        /// as well as whether or not the player is currently climbing anything.
+        /// Calculates and returns the move vector that the player wants to move the character in, regardless of whether or not they're currently jumping or climbing.
+        /// 
         /// </summary>
         /// <param name="relativeTo">The Vector3 that the forward, backward, left, and right values are relative to</param>
         /// <returns>The calculated move vector</returns>
@@ -62,5 +66,7 @@ namespace Jumpvalley.Players.Movement
         {
             return new Vector3(0, 0, 0);
         }
+
+        
     }
 }
