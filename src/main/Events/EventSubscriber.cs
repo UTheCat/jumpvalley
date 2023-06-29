1d3f79583408bcdf9c88255e63f6046752779210
udev <83146800+UTheDev@users.noreply.github.com>
2023-06-29 01:22:39 -0700

    Update EventSubscriber.cs

--- src/main/Events/EventSubscriber.cs
+++ src/main/Events/EventSubscriber.cs
@@ -9,23 +9,52 @@ namespace Jumpvalley.Events
 {
     /// <summary>
     /// Utility class for safely connecting and disconnecting functions from events.
-    /// The implementation of this class is currently not finished.
+    /// <br/>
+    /// It helps to prevent duplicate event connections, and it checks if the event connection is active before disconnecting.
+    /// <br/>
+    /// The implementation of this class is currently unfinished.
     /// </summary>
     /// <typeparam name="TEventArgs">The type of the event arguments</typeparam>
     public partial class EventSubscriber<TEventArgs>
     {
+        /// <summary>
+        /// The event that the event subscriber is connected to.
+        /// </summary>
         public event EventHandler<TEventArgs> Event;
 
-        public EventSubscriber(EventHandler<TEventArgs> @event, Callback callback)
-        {
-            Event = @event;
-        }
-
         /// <summary>
-        /// The callback to connect the event to.
+        /// The callback that will get connected to the event.
         /// </summary>
         /// <param name="o">The object that the event call comes from</param>
         /// <param name="e">The event data</param>
-        public delegate void Callback(object o, TEventArgs e);
+        public EventHandler<TEventArgs> Callback;
+
+        private bool _isConnected = false;
+
+        /// <summary>
+        /// Whether or not <see cref="Callback"/> is connected to <see cref="Event"/>.
+        /// <br/>
+        /// Setting this to true will connect the currently specified callback to the currently specified event.
+        /// <br/>
+        /// Setting this to false will disconnect the currently specified callback from the currently specified event.
+        /// </summary>
+        public bool IsConnected
+        {
+            get => _isConnected;
+            set
+            {
+                if (value == _isConnected) return;
+
+                if (value)
+                {
+                    Event += Callback;
+                }
+            }
+        }
+
+        public EventSubscriber(EventHandler<TEventArgs> callback)
+        {
+            Callback = callback;
+        }
     }
 }
