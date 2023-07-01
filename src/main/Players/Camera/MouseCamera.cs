5025794215d4707ce4b9028f85f8ef624579d848
udev <83146800+UTheDev@users.noreply.github.com>
2023-06-30 20:48:44 -0700

    Update MouseCamera.cs

--- src/main/Players/Camera/MouseCamera.cs
+++ src/main/Players/Camera/MouseCamera.cs
@@ -10,14 +10,29 @@ namespace Jumpvalley.Players.Camera
     /// <summary>
     /// Handles control of the player's camera via a mouse
     /// </summary>
-    public partial class MouseCamera: BaseCamera
+    public partial class MouseCamera: BaseCamera, IDisposable
     {
+        private static readonly string INPUT_CAMERA_PAN = "camera_pan";
+
+        private bool isTurningCamera = false;
+
         public MouseCamera() : base() { }
 
         public override void _Input(InputEvent @event)
         {
+            if (Input.IsActionJustPressed(INPUT_CAMERA_PAN))
+            {
+                isTurningCamera = true;
+                Input.MouseMode = Input.MouseModeEnum.Captured;
+            }
+            else if (Input.IsActionJustReleased(INPUT_CAMERA_PAN))
+            {
+                isTurningCamera = false;
+                Input.MouseMode = Input.MouseModeEnum.Visible;
+            }
+
             // Right-click to turn the camera
-            if (@event is InputEventMouseMotion mouseEvent && mouseEvent.ButtonMask == MouseButtonMask.Right)
+            if (isTurningCamera && @event is InputEventMouseMotion mouseEvent)
             {
                 Vector2 mouseEventRelative = mouseEvent.Relative;
 
@@ -27,6 +42,14 @@ namespace Jumpvalley.Players.Camera
             }
 
             base._Input(@event);
+        }
+
+        public new void Dispose()
+        {
+            SetProcessInput(false);
+            isTurningCamera = false;
+
+            base.Dispose();
         }
     }
 }
