0f44d8cefd8d4873db09fc6c610df94111b33508
udev <83146800+UTheDev@users.noreply.github.com>
2023-07-05 18:08:11 -0700

    Finish first version of MusicZone

--- src/main/Music/MusicZone.cs
+++ src/main/Music/MusicZone.cs
@@ -1,3 +1,7 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
 /// <summary>
 /// Represents a single music zone
 /// <br/>
@@ -5,8 +9,57 @@
 /// </summary>
 namespace Jumpvalley.Music
 {
-    public partial class MusicZone
+    public partial class MusicZone: MusicGroup
     {
+        /// <summary>
+        /// The name of the <see cref="Node3D"/>s that make up the geometry of the music zone
+        /// </summary>
+        public static readonly string MUSIC_ZONE_GEOMETRY_NAME = "ZonePart";
 
+        /// <summary>
+        /// The boxes that make up the geometry of the music zone.
+        /// </summary>
+        public List<Node3D> boxes = new List<Node3D>();
+
+        public MusicZone(Node node) : base(node)
+        {
+            Godot.Collections.Array<Node> nodes = GetChildren();
+
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                if (nodes[i] is Node3D box && box.Name.Equals(MUSIC_ZONE_GEOMETRY_NAME))
+                {
+                    boxes.Add(box);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Based on the size, rotation, and position of each box that makes up the music zone, this method returns whether or not the given
+        /// point is within the music zone.
+        /// </summary>
+        /// <param name="point">The point (in global space coordinates)</param>
+        /// <returns>Whether or not the point is in the music zone</returns>
+        public bool IsPointInZone(Vector3 point)
+        {
+            for (int i = 0; i < boxes.Count; i++)
+            {
+                Node3D box = boxes[i];
+                Vector3 boxSize = box.Scale;
+
+                // Convert the given point to object space
+                point = box.ToLocal(point);
+
+                // Check if the point is inside the box
+                if (Math.Abs(point.X) <= boxSize.X / 2
+                    && Math.Abs(point.Y) <= boxSize.Y / 2
+                    && Math.Abs(point.Z) <= boxSize.Z / 2)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
