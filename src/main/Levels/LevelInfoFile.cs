8b72e4bcc144813f14700b055cb6b415a173c9f4
UTheDev <83146800+UTheDev@users.noreply.github.com>
2023-07-28 14:40:51 -0700

    Create LevelInfoFile
    This new class handles info files associated with levels.

---
+++ src/main/Levels/LevelInfoFile.cs
@@ -0,0 +1,47 @@
+﻿using Godot;
+using System;
+using System.IO;
+
+namespace Jumpvalley.Levels
+{
+    /// <summary>
+    /// Reads an info file for a Jumpvalley level
+    /// </summary>
+    public partial class LevelInfoFile: IO.InfoFile
+    {
+        /// <summary>
+        /// The full name of the level. This is the name of the level that will actually be displayed to the user, and it can be different from the name of the level's root node.
+        /// </summary>
+        public string FullName = "";
+
+        /// <summary>
+        /// How difficult the level is.
+        /// </summary>
+        public Difficulty LevelDifficulty;
+
+        public LevelInfoFile(string text): base(text)
+        {
+            Data.TryGetValue("full_name", out FullName);
+
+            string difficultyText;
+
+            // If TryGetValue failed, it will return false
+            if (!Data.TryGetValue("difficulty", out difficultyText))
+            {
+                throw new InvalidDataException("The level's info file doesn't specify the difficulty of the level, or the info file is improperly formatted.");
+            }
+
+            double difficultyRating;
+
+            // If double.TryParse failed, it will return false
+            if (!double.TryParse(difficultyText, out difficultyRating))
+            {
+                throw new InvalidDataException("Difficulty specified in the level's info file isn't a number.");
+            }
+
+            // We'll need to retain the exact numerical difficulty when setting LevelDifficulty
+            Difficulty difficulty = DifficultyPresets.GetPrimaryDifficultyFromRating(difficultyRating);
+            LevelDifficulty = new Difficulty(difficulty.Name, difficultyRating, difficulty.Color);
+        }
+    }
+}
