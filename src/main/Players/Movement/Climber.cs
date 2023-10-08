b3f9f97d6aa017f45e9679c39b110cb7cf7a960d
UTheDev <83146800+UTheDev@users.noreply.github.com>
2023-10-08 10:12:48 -0700

    Create Climber.cs

---
+++ src/main/Players/Movement/Climber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jumpvalley.Players.Movement
+{
+    /// <summary>
+    /// Allows a character to climb objects.
+    /// A MeshInstance3D can be climbed if it has a boolean metadata entry named "Climbable" that's set to true.
+    /// </summary>
+    public partial class Climber
+    {
+        public Climber() { }
+    }
+}
