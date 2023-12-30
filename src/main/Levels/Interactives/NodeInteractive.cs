3d5e0d1d6b3906a53880f7f27c500a9ec3c20889
UTheDev <83146800+UTheDev@users.noreply.github.com>
2023-12-29 18:47:47 -0800

    Update NodeInteractive.cs

--- src/main/Levels/Interactives/NodeInteractive.cs
+++ src/main/Levels/Interactives/NodeInteractive.cs
@@ -1,10 +1,17 @@
-﻿namespace Jumpvalley.Levels.Interactives
+﻿using Godot;
+using System.Diagnostics;
+
+namespace Jumpvalley.Levels.Interactives
 {
     /// <summary>
     /// A subclass of <see cref="Interactive"/> that operates over a Godot node.
     /// It makes using a Godot node's properties and metadata easier and provides events to respond to changes in a node's properties or attributes.
     /// </summary>
-    public partial class NodeInteractive
+    public partial class NodeInteractive: Interactive
     {
+        public NodeInteractive(Stopwatch stopwatch, Node node) : base(stopwatch)
+        {
+
+        }
     }
 }
