43372a8649ab4583c4dd9b3aabb18a12eb9ec86f
UTheDev <83146800+UTheDev@users.noreply.github.com>
2023-12-29 18:46:29 -0800

    Create NodeInteractive.cs

---
+++ src/main/Levels/Interactives/NodeInteractive.cs
@@ -0,0 +1,10 @@
+﻿namespace Jumpvalley.Levels.Interactives
+{
+    /// <summary>
+    /// A subclass of <see cref="Interactive"/> that operates over a Godot node.
+    /// It makes using a Godot node's properties and metadata easier and provides events to respond to changes in a node's properties or attributes.
+    /// </summary>
+    public partial class NodeInteractive
+    {
+    }
+}
