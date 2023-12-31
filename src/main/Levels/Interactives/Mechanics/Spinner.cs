0c0991d9381b9b0ea81767f7b627f721b1b92115
UTheDev <83146800+UTheDev@users.noreply.github.com>
2023-12-30 16:45:47 -0800

    make Spinner use built-in properties

--- src/main/Levels/Interactives/Mechanics/Spinner.cs
+++ src/main/Levels/Interactives/Mechanics/Spinner.cs
@@ -10,36 +10,22 @@ namespace Jumpvalley.Levels.Interactives.Mechanics
     public partial class Spinner : InteractiveNode, IDisposable
     {
         private StaticBody3D body;
-        private Vector3 _angularVelocity;
-
-        /// <summary>
-        /// The constant angular velocity of the spinner (the number of radians rotated per second for pitch, yaw, and roll).
-        /// </summary>
-        public Vector3 AngularVelocity
-        {
-            get => _angularVelocity;
-            set
-            {
-                _angularVelocity = value;
-                body.ConstantAngularVelocity = value;
-            }
-        }
 
         public Spinner(Stopwatch stopwatch, Node node) : base(stopwatch, node)
         {
             body = node as StaticBody3D;
             if (body == null) throw new ArgumentException("The node specified in the 2nd argument must be a StaticBody3D.");
 
-            AngularVelocity = body.ConstantAngularVelocity;
             body.AddChild(this);
         }
 
         private void Update(float delta)
         {
+            Vector3 angularVelocity = body.ConstantAngularVelocity;
             body.Rotation += new Vector3(
-                AngularVelocity.X * delta,
-                AngularVelocity.Y * delta,
-                AngularVelocity.Z * delta
+                angularVelocity.X * delta,
+                angularVelocity.Y * delta,
+                angularVelocity.Z * delta
                 );
         }
 
