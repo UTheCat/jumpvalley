a46d179401040a3138b16a9e61a5180774bc4b8e
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-01-01 19:23:15 -0800

    add IsRunning property to Interactive

--- src/main/Levels/Interactives/Interactive.cs
+++ src/main/Levels/Interactives/Interactive.cs
@@ -15,6 +15,11 @@ namespace Jumpvalley.Levels.Interactives
         public bool IsInitialized { get; protected set; }
 
         /// <summary>
+        /// Whether or not the interactive is running (e.g. after <see cref="Start"/> has been called).
+        /// </summary>
+        public bool IsRunning { get; protected set; }
+
+        /// <summary>
         /// The <see cref="Stopwatch"/> that the Interactive will run on.
         /// This Stopwatch can be used to synchronize the Interactive's various operations to whatever the current time
         /// on the Stopwatch is.
@@ -50,12 +55,22 @@ namespace Jumpvalley.Levels.Interactives
         /// The interactive's start method. This method is called every time the user starts or restarts the interactive,
         /// and it's a great place to put code that will be run after initialization, but just before the interactive starts.
         /// </summary>
-        public virtual void Start() { }
+        public virtual void Start()
+        {
+            if (IsRunning) return;
+
+            IsRunning = true;
+        }
 
         /// <summary>
         /// The interactive's stop method. This method is called right after the user stops or exits the interactive.
         /// </summary>
-        public virtual void Stop() { }
+        public virtual void Stop()
+        {
+            if (!IsRunning) return;
+
+            IsRunning = false;
+        }
 
         /// <summary>
         /// Disposes of this <see cref="Interactive"/> instance. This method is a great place to free up resources being used by the interactive instance,
