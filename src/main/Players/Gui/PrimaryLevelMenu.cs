31d11c8b905ed424ad6b199b460128f62a9214b7
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-01-06 18:41:26 -0800

    Create PrimaryLevelMenu.cs

---
+++ src/main/Players/Gui/PrimaryLevelMenu.cs
@@ -0,0 +1,16 @@
+﻿using Godot;
+
+namespace Jumpvalley.Players.Gui
+{
+    /// <summary>
+    /// The primary level menu, the menu that's accessed by pressing the three-dots at the bottom of the user's screen.
+    /// This menu contains options like accessing game settings and exiting the game.
+    /// </summary>
+    public partial class PrimaryLevelMenu: LevelMenu
+    {
+        public PrimaryLevelMenu(Control actualNode, SceneTree tree) : base(actualNode, tree)
+        {
+
+        }
+    }
+}
