962352cae7ac6a69f78239cd51db7bdec3de43e7
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-01-20 19:13:16 -0800

    add InfoFile testing class

---
+++ src/main/Testing/InfoFileTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Jumpvalley.IO;
+
+namespace Jumpvalley.Testing
+{
+    /// <summary>
+    /// Test class for making sure the <see cref="InfoFile"/> class works properly
+    /// </summary>
+    public partial class InfoFileTest
+    {
+        private InfoFile infoFile;
+        private string name;
+
+        public InfoFileTest(string infoFileText, string infoFileName = "test info file")
+        {
+            infoFile = new InfoFile(infoFileText);
+            name = infoFileName;
+        }
+
+        public void PrintInfoFileData()
+        {
+            string printedInfoFileData = "";
+
+            foreach (KeyValuePair<string, string> property in infoFile.Data)
+            {
+                printedInfoFileData += $"- The '{property.Key}' property has the value '{property.Value}'\n";
+            }
+
+            Console.WriteLine($"Test info file named '{name}' has these contents:\n{printedInfoFileData}");
+        }
+    }
+}
