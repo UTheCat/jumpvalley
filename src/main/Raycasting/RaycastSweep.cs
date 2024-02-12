07249c1f897e3495ecb5a3a138a7b8ce7e536344
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-02-11 20:35:30 -0800

    Create RaycastSweep.cs

---
+++ src/main/Raycasting/RaycastSweep.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jumpvalley.Raycasting
+{
+    /// <summary>
+    /// Sweeps over a region of 3D space using a series of equally-spaced-out raycasts.
+    /// </summary>
+    public partial class RaycastSweep
+    {
+    }
+}
