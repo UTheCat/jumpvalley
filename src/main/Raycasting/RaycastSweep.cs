3df05a85366767c6dcc196d3a745f7d30dbcd5bd
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-02-11 20:41:29 -0800

    add variables to RaycastSweep

--- src/main/Raycasting/RaycastSweep.cs
+++ src/main/Raycasting/RaycastSweep.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Godot;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,24 @@ namespace Jumpvalley.Raycasting
     /// </summary>
     public partial class RaycastSweep
     {
+        /// <summary>
+        /// The total number of raycasts that the raycast sweep will perform from the start position to the end position.
+        /// </summary>
+        public int NumRaycasts { get; private set; }
+
+        /// <summary>
+        /// The starting position of the raycast sweep. This is where the first raycast will begin.
+        /// </summary>
+        public Vector3 StartPosition { get; private set; }
+
+        /// <summary>
+        /// The ending position of the raycast sweep. This is where the last raycast will begin.
+        /// </summary>
+        public Vector3 EndPosition { get; private set; }
+
+        public RaycastSweep()
+        {
+
+        }
     }
 }
