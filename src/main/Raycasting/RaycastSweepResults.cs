1e734a9dc125c9e8a87f7d127bc4b9724e68fbb6
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-02-16 23:24:09 -0800

    Create RaycastSweepResults.cs

---
+++ src/main/Raycasting/RaycastSweepResults.cs
@@ -0,0 +1,33 @@
+﻿using Godot;
+
+namespace Jumpvalley.Raycasting
+{
+    /// <summary>
+    /// Class that contains data about the result of a raycast sweep operation
+    /// (where collisions with a <see cref="RaycastSweep"/>'s raycasts are checked).
+    /// </summary>
+    public partial class RaycastSweepResults
+    {
+        /// <summary>
+        /// The raycast that got collided with.
+        /// </summary>
+        public RayCast3D Raycast;
+
+        /// <summary>
+        /// The global-space position where the raycast hit an object.
+        /// </summary>
+        public Vector3 CollisionPoint;
+
+        /// <summary>
+        /// The <see cref="Node3D"/> that the raycast hit.
+        /// </summary>
+        public Node3D CollidedObject;
+
+        public RaycastSweepResults(RayCast3D raycast, Vector3 collisionPoint, Node3D collidedObject)
+        {
+            Raycast = raycast;
+            CollisionPoint = collisionPoint;
+            CollidedObject = collidedObject;
+        }
+    }
+}
