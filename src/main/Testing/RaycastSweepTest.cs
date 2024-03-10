e491e97f9bfac8e54e21f4e4ecfb6e46075fd89d
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-03-09 23:10:39 -0800

    add test class for RaycastSweep

---
+++ src/main/Testing/RaycastSweepTest.cs
@@ -0,0 +1,51 @@
+﻿using Godot;
+using System;
+
+using Jumpvalley.Raycasting;
+
+namespace Jumpvalley.Testing
+{
+    /// <summary>
+    /// Test class for making sure the <see cref="RaycastSweep"/> class works properly
+    /// </summary>
+    public partial class RaycastSweepTest: Node
+    {
+        public RaycastSweep TestRaycastSweep;
+        public Label RaycastResultLabel;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="RaycastSweepTest"/>
+        /// </summary>
+        /// <param name="raycastSweep">The raycast sweep to apply</param>
+        /// <param name="node">The Node3D to use the raycast sweep for</param>
+        public RaycastSweepTest(RaycastSweep raycastSweep, Node3D node)
+        {
+            if (node == null) throw new ArgumentNullException("node");
+
+            RaycastResultLabel = new Label();
+
+            TestRaycastSweep = raycastSweep;
+            node.AddChild(raycastSweep);
+        }
+
+        public override void _PhysicsProcess(double delta)
+        {
+            base._PhysicsProcess(delta);
+
+            RaycastSweepResult results = TestRaycastSweep.PerformRaycast();
+
+            string resultText = $"RaycastSweep results for {RaycastResultLabel.Name}\n";
+            
+            if (results == null)
+            {
+                resultText += "Currently not colliding with anything";
+            }
+            else
+            {
+                resultText += $"Collided object: {results.CollidedObject}\nCollision point: {results.CollisionPoint}\nColliding raycast: {results.Raycast}\nColliding raycast index: {results.RaycastIndex}";
+            }
+
+            RaycastResultLabel.Text = resultText;
+        }
+    }
+}
