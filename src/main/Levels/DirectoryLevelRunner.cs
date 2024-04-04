54649e7a07fc11e22a9bbbfb3e768fc0eff1c442
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-04-03 18:36:01 -0700

    Update DirectoryLevelRunner.cs

--- src/main/Levels/DirectoryLevelRunner.cs
+++ src/main/Levels/DirectoryLevelRunner.cs
@@ -5,16 +5,21 @@ namespace Jumpvalley.Levels
     /// <summary>
     /// Subclass of <see cref="LevelRunner"/> that handles running levels located within a directory. 
     /// </summary>
-    public partial class DirectoryLevelRunner: LevelRunner
+    public partial class DirectoryLevelRunner : LevelRunner
     {
         /// <summary>
         /// File path to the directory containing the levels
         /// </summary>
         public string DirectoryPath { get; private set; }
 
-        public DirectoryLevelRunner(Player player, string directoryPath): base(player)
+        /// <summary>
+        /// Creates a new <see cref="DirectoryLevelRunner"/>
+        /// </summary>
+        /// <param name="player">The player instance to run this level runner under</param>
+        /// <param name="directoryPath">Path to the directory containing the levels</param>
+        public DirectoryLevelRunner(Player player, string directoryPath) : base(player)
         {
-
+            
         }
     }
 }
