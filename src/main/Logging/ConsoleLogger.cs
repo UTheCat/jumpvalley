df3c9477c5d53bf09c47f4c159147ad0a3a3683a
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-04-05 18:53:28 -0700

    Update ConsoleLogger.cs

--- src/main/Logging/ConsoleLogger.cs
+++ src/main/Logging/ConsoleLogger.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 namespace Jumpvalley.Logging
@@ -37,9 +38,10 @@ namespace Jumpvalley.Logging
         /// Creates a new instance of <see cref="ConsoleLogger"/> for a given printing "source".
         /// </summary>
         /// <param name="source">Where the output is coming from</param>
-        public ConsoleLogger(string source)
+        public ConsoleLogger(string source = "", PrintingApi api = PrintingApi.Standard)
         {
             Source = source;
+            Api = api;
         }
 
         /// <summary>
@@ -49,6 +51,21 @@ namespace Jumpvalley.Logging
         public void Print(string message)
         {
             PrintingApi api = Api;
+            TimeSpan currentTime = TimeSpan.FromMilliseconds(Time.GetTicksMsec());
+
+            // Time displayed here is formatted based on the information in this article:
+            // https://learn.microsoft.com/en-us/dotnet/standard/base-types/standard-timespan-format-strings
+            string actualMessage = $"[{string.Format("{0:G}", currentTime)}][{Source}]: {message}";
+
+            switch (api)
+            {
+                case PrintingApi.Standard:
+                    Console.WriteLine("");
+                    break;
+                case PrintingApi.Godot:
+                    GD.Print("");
+                    break;
+            }
         }
     }
 }
