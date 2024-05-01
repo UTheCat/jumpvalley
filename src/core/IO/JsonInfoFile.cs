e861ac74d3660f7577e5f24bdd7da5f721804379
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-04-30 18:45:03 -0700

    Update JsonInfoFile.cs

--- src/core/IO/JsonInfoFile.cs
+++ src/core/IO/JsonInfoFile.cs
@@ -3,7 +3,7 @@ using System.Text.Json;
 namespace Jumpvalley.IO
 {
     /// <summary>
-    /// Class that handles info files written in JavaScript Object Notation (JSON).
+    /// Class intended to help with using the data from info files written in JavaScript Object Notation (JSON).
     /// <br/><br/>
     /// Info files are files that contain metadata about an object (e.g. one that's on a filesystem).
     /// </summary>
@@ -14,9 +14,18 @@ namespace Jumpvalley.IO
         /// </summary>
         public static readonly string FILE_NAME = "info.json";
 
+        /// <summary>
+        /// The raw JSON text from the JSON info file
+        /// </summary>
+        public string RawJson;
+
+        /// <summary>
+        /// Reads a JSON info file from its raw text and stores such data in the <see cref="RawData"/> field.
+        /// </summary>
+        /// <param name="json"></param>
         public JsonInfoFile(string json)
         {
-
+            RawJson = json;
         }
     }
 }
