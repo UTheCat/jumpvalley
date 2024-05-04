1646ad28f335ad6e842e92b5eb9e131ecd014e22
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-05-04 14:32:42 -0700

    have SongPackage use info.json

--- src/core/Music/SongPackage.cs
+++ src/core/Music/SongPackage.cs
@@ -1,6 +1,7 @@
 ﻿using Godot;
 using System;
-//using System.IO;
+using System.IO;
+using System.Text.Json.Nodes;
 
 namespace Jumpvalley.Music
 {
@@ -9,27 +10,22 @@ namespace Jumpvalley.Music
     /// </summary>
     public class SongPackage
     {
-        private static readonly string INFO_FILE_NAME = IO.InfoFile.FILE_NAME;
+        private static readonly string INFO_FILE_NAME = IO.JsonInfoFile.FILE_NAME;
 
         /// <summary>
-        /// Data as raw text that comes from "info.txt" within the folder
+        /// The data of the corresponding "info.json" file
         /// </summary>
-        public string InfoText = "";
-
-        /// <summary>
-        /// The data of the corresponding "info.txt" file
-        /// </summary>
-        public SongInfoFile InfoFile = null;
+        public SongInfo Info;
 
         /// <summary>
         /// The file path to the song's audio file
         /// </summary>
-        public string SongPath;
+        public string AudioPath;
 
         /// <summary>
-        /// The directory path of this <see cref="SongPackage"/> with trailing slashes and trailing backslashes removed
+        /// The directory path of this <see cref="SongPackage"/>
         /// </summary>
-        public string Path = "";
+        public string DirPath;
 
         /// <summary>
         /// Creates a SongPackage instance for a given folder path
@@ -37,18 +33,10 @@ namespace Jumpvalley.Music
         /// <param name="path">The folder path</param>
         public SongPackage(string path)
         {
+            DirPath = path;
+
             if (!string.IsNullOrEmpty(path))
             {
-                // Some like to end the paths with an extra "/" or "\". Account for this.
-                if (path.EndsWith("/") || path.EndsWith("\\"))
-                {
-                    path = path.Substring(0, path.Length - 1);
-                }
-
-                Path = path;
-
-                //Console.WriteLine("Try opening directory: " + path);
-
                 // check if the directory can be opened first
                 DirAccess dir = DirAccess.Open(path);
                 if (dir == null)
@@ -56,25 +44,22 @@ namespace Jumpvalley.Music
                     throw new Exception($"Directory access failed. This is the message returned by DirAccess.GetOpenError(): {DirAccess.GetOpenError()}");
                 }
 
-                path += "/";
-
                 //Console.WriteLine("Try opening info file");
 
                 // if so, try opening the files inside
-                using Godot.FileAccess infoFile = Godot.FileAccess.Open(path + INFO_FILE_NAME, Godot.FileAccess.ModeFlags.Read);
+                using Godot.FileAccess infoFile = Godot.FileAccess.Open(Path.Combine(path, INFO_FILE_NAME), Godot.FileAccess.ModeFlags.Read);
                 if (infoFile == null)
                 {
-                    throw new Exception($"Failed to open the corresponding {INFO_FILE_NAME} file. This is the message returned by FileAccess.GetOpenError(): {Godot.FileAccess.GetOpenError()}");
+                    throw new Exception($"Failed to open the corresponding {INFO_FILE_NAME} file. This is the message returned by Godot.FileAccess.GetOpenError(): {Godot.FileAccess.GetOpenError()}");
                 }
 
                 //Console.WriteLine("Try reading the info file");
 
                 // retrieve the info from the info file
                 string infoText = infoFile.GetAsText();
-                InfoText = infoText;
-                InfoFile = new SongInfoFile(infoText);
+                Info = new SongInfo(JsonNode.Parse(infoText));
 
-                SongPath = InfoFile.AudioPath;
+                AudioPath = Info.AudioPath;
             }
         }
     }
