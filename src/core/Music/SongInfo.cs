02c7f73f4f4e0673398dcb47c8c2a31a0127eecf
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-05-04 12:56:25 -0700

    Create SongInfo.cs

---
+++ src/core/Music/SongInfo.cs
@@ -0,0 +1,12 @@
+using Jumpvalley.IO;
+
+namespace Jumpvalley.Music
+{
+    /// <summary>
+    /// Class that holds some metadata about a song file.
+    /// </summary>
+    public partial class SongInfo : JsonInfoFile
+    {
+        
+    }
+}
