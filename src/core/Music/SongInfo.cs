0284c2b19e0266a28f5ef81b0478b4525b4e8fe7
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-05-04 13:17:16 -0700

    Update SongInfo.cs

--- src/core/Music/SongInfo.cs
+++ src/core/Music/SongInfo.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Nodes;
 using Jumpvalley.IO;
 
 namespace Jumpvalley.Music
@@ -7,6 +8,32 @@ namespace Jumpvalley.Music
     /// </summary>
     public partial class SongInfo : JsonInfoFile
     {
-        
+        /// <summary>
+        /// The name of the song
+        /// </summary>
+        public string Name = null;
+
+        /// <summary>
+        /// The artists that made the song
+        /// </summary>
+        public string Artists = null;
+
+        /// <summary>
+        /// The album the song belongs to
+        /// </summary>
+        public string Album = null;
+
+        /// <summary>
+        /// The file path to the audio file, including its file name and extension
+        /// </summary>
+        public string AudioPath = null;
+
+        public SongInfo(JsonNode data) : base(data)
+        {
+            Name = data["name"]?.GetValue<string>();
+            Artists = data["artists"]?.GetValue<string>();
+            Album = data["album"]?.GetValue<string>();
+            AudioPath = data["audioPath"]?.GetValue<string>();
+        }
     }
 }
