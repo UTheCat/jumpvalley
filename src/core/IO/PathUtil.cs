df828cbce73740d931a8883991839fb16bac75c7
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-05-05 12:52:31 -0700

    Create PathUtil.cs

---
+++ src/core/IO/PathUtil.cs
@@ -0,0 +1,44 @@
+namespace Jumpvalley.IO
+{
+    /// <summary>
+    /// Provides some methods for performing operations related to file paths.
+    /// </summary>
+    public partial class PathUtil
+    {
+        /// <summary>
+        /// The conventional directory separator on Windows,
+        /// which is a backslash.
+        /// </summary>
+        public static readonly char WINDOWS_DIR_SEPARATOR = '\\';
+
+        /// <summary>
+        /// The conventional directory separator on Godot,
+        /// which is a forward slash.
+        /// </summary>
+        public static readonly char GODOT_DIR_SEPARATOR = '/';
+
+        /// <summary>
+        /// Concatenates multiple paths into one using
+        /// Godot's conventional file path format 
+        /// (where the directory separator is a forward slash).
+        /// </summary>
+        /// <param name="paths">The file paths to combine</param>
+        /// <returns>
+        /// A file path that follows Godot's conventional file path format.
+        /// </returns>
+        public static string GodotCombine(params string[] paths)
+        {
+            string finalPath = "";
+
+            for (int i = 0; i < paths.Length; i++)
+            {
+                string path = paths[i];
+
+                path.Replace(WINDOWS_DIR_SEPARATOR, GODOT_DIR_SEPARATOR);
+                finalPath += path;
+            }
+
+            return finalPath;
+        }
+    }
+}
