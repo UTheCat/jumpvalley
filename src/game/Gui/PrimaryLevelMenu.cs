53b2261924b979ab3f5cdeb91148fc1561c947a9
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-05-20 12:23:39 -0700

    put settings button in the primary level menu

--- src/game/Gui/PrimaryLevelMenu.cs
+++ src/game/Gui/PrimaryLevelMenu.cs
@@ -1,7 +1,5 @@
 using Godot;
 
-using JumpvalleyGame.Gui;
-
 namespace JumpvalleyGame.Gui
 {
 	/// <summary>
@@ -10,6 +8,8 @@ namespace JumpvalleyGame.Gui
 	/// </summary>
 	public partial class PrimaryLevelMenu : LevelMenu
 	{
+		private readonly int BUTTON_Y_POS_DIFF = 52;
+
 		public PrimaryLevelMenu(Control actualNode, SceneTree tree) : base(actualNode, tree)
 		{
 			if (TitleLabel != null)
@@ -23,6 +23,14 @@ namespace JumpvalleyGame.Gui
 			}
 
 			PackedScene menuButtonResource = ResourceLoader.Load<PackedScene>("res://gui/level_menu_button.tscn");
+
+			// Add the Settings button
+			LevelMenuButton settingsButton = new LevelMenuButton(menuButtonResource.Instantiate<Button>())
+			{
+				Text = actualNode.Tr("SETTINGS_MENU_TITLE"),
+				Icon = GD.Load<CompressedTexture2D>("res://addons/icons/settings_48dp.svg"),
+				BackgroundColor = Color.FromHsv(48f / 360f, 0.65f, 1f)
+			};
 
 			// Add the Exit Game button
 			Button exitGameButton = menuButtonResource.Instantiate<Button>();
@@ -40,7 +48,17 @@ namespace JumpvalleyGame.Gui
 
 			menuButtonResource.Dispose();
 
-			ItemsControl.AddChild(exitGameButton);
+			Button[] buttonList = {
+				settingsButton.ActualButton,
+				exitGameButton
+			};
+
+			for (int i = 0; i < buttonList.Length; i++)
+			{
+				Button b = buttonList[i];
+				b.Position = new Vector2(0, BUTTON_Y_POS_DIFF * i);
+				ItemsControl.AddChild(b);
+			}
 		}
 	}
 }
