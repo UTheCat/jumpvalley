6997f5ab31c36e48ad90b5180cd90f397d8dab18
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-05-21 15:16:22 -0700

    Create SettingGroup.cs

---
+++ src/game/Settings/SettingGroup.cs
@@ -0,0 +1,10 @@
+namespace JumpvalleyGame.Settings
+{
+    /// <summary>
+    /// Class that represents a group/category of settings
+    /// </summary>
+    public partial class SettingGroup
+    {
+
+    }
+}
