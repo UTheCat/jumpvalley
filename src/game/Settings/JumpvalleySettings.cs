453ab638f9f8f725d46038425abe70c646f970a3
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-05-24 15:58:04 -0700

    more work on settings stuff

---
+++ src/game/Settings/JumpvalleySettings.cs
@@ -0,0 +1,15 @@
+namespace JumpvalleyGame.Settings
+{
+    /// <summary>
+    /// Main list of user settings for Jumpvalley
+    /// </summary>
+    public partial class JumpvalleySettings
+    {
+        public SettingGroup Group;
+
+        public JumpvalleySettings()
+        {
+            SettingGroup group = new SettingGroup();
+        }
+    }
+}
--- src/game/Settings/SettingGroup.cs
+++ src/game/Settings/SettingGroup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace JumpvalleyGame.Settings
@@ -5,10 +6,69 @@ namespace JumpvalleyGame.Settings
     /// <summary>
     /// Class that represents a group/category of settings
     /// </summary>
-    public partial class SettingGroup
+    public partial class SettingGroup : IDisposable
     {
         public string LocalizationId;
         public List<SettingGroup> Subgroups;
         public List<SettingBase<object>> SettingList;
+
+        /// <summary>
+        /// Removes a setting from the setting list
+        /// </summary>
+        /// <param name="setting"></param>
+        public void Remove(SettingBase<object> setting)
+        {
+            if (setting != null)
+            {
+                int index = SettingList.IndexOf(setting);
+                if (index >= 0)
+                {
+                    SettingList.Remove(setting);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds a setting to the setting list
+        /// </summary>
+        /// <param name="setting"></param>
+        public void Add(SettingBase<object> setting)
+        {
+            if (setting == null) return;
+
+            SettingList.Add(setting);
+            setting.Changed += HandleSettingChanged;
+        }
+
+        public void Dispose()
+        {
+            foreach (SettingBase<object> s in SettingList)
+            {
+                s.Changed -= HandleSettingChanged;
+            }
+
+            SettingList.Clear();
+        }
+
+        private void HandleSettingChanged(object o, EventArgs _e)
+        {
+            SettingBase<object> setting = o as SettingBase<object>;
+            if (setting != null)
+            {
+                RaiseSettingChanged(setting);
+            }
+        }
+
+        /// <summary>
+        /// Fired when one of the setting group's settings changes
+        /// <br/>
+        /// The EventArgs parameter is the setting that was changed as a <see cref="SettingBase{T}"/>. 
+        /// </summary>
+        public event EventHandler<SettingBase<object>> SettingChanged;
+
+        protected void RaiseSettingChanged(SettingBase<object> setting)
+        {
+            SettingChanged?.Invoke(this, setting);
+        }
     }
 }
