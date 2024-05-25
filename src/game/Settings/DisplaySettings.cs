9182eced90339ea61c2b5d146e27bc34e75076eb
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-05-24 17:13:48 -0700

    Create DisplaySettings.cs

---
+++ src/game/Settings/DisplaySettings.cs
@@ -0,0 +1,28 @@
+namespace JumpvalleyGame.Settings
+{
+    /// <summary>
+    /// Display settings for Jumpvalley
+    /// </summary>
+    public partial class DisplaySettings : SettingGroup
+    {
+        public DisplaySettings()
+        {
+            Id = "display";
+            LocalizationId = "SETTINGS_GROUP_DISPLAY";
+
+            SettingBase[] settings = [
+                new SettingBase()
+                {
+                    Id = "fullscreen",
+                    LocalizationId = "SETTINGS_FULLSCREEN",
+                    Value = false
+                }
+            ];
+
+            foreach (SettingBase s in settings)
+            {
+                Add(s);
+            }
+        }
+    }
+}
