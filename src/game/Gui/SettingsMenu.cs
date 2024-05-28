b3a66d1f5883a46b1c6fe19f130f12c13da787da
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-05-27 20:22:55 -0700

    implement SettingsMenu visibility stuff

--- src/game/Gui/SettingsMenu.cs
+++ src/game/Gui/SettingsMenu.cs
@@ -1,6 +1,7 @@
 using System;
 using Godot;
 using Jumpvalley.Animation;
+using Jumpvalley.Tweening;
 
 using JumpvalleyGame.Settings;
 
@@ -11,18 +12,61 @@ namespace JumpvalleyGame.Gui
     /// </summary>
     public partial class SettingsMenu : AnimatedNode, IDisposable
     {
+        private Control menu;
         private SettingGroup settings;
         private Control settingListNode;
         private PackedScene categoryScene;
         private PackedScene checkButtonSettingScene;
 
-        public SettingsMenu(Node actualNode, SettingGroup settings) : base(actualNode)
+        private SceneTreeTween positionTween;
+
+        public override bool IsVisible
         {
+            get => base.IsVisible;
+            set
+            {
+                base.IsVisible = value;
+
+                if (value)
+                {
+                    positionTween.Speed = 1f;
+                }
+                else
+                {
+                    positionTween.Speed = -1f;
+                }
+
+                positionTween.Resume();
+            }
+        }
+
+        public SettingsMenu(Control actualNode, SceneTree tree, SettingGroup settings) : base(actualNode)
+        {
+            menu = actualNode;
             this.settings = settings;
             settingListNode = actualNode.GetNode<Control>("ScrollContainer/VBoxContainer/SettingList");
 
             categoryScene = LoadSettingNodeScene("setting_category_scene");
             checkButtonSettingScene = LoadSettingNodeScene("check_button_setting_scene");
+
+            menu.Visible = false;
+            SetMenuPosition(1f);
+
+            positionTween = new SceneTreeTween(0.25, Tween.TransitionType.Quad, Tween.EaseType.Out, tree);
+            positionTween.InitialValue = 1;
+            positionTween.FinalValue = 0;
+            positionTween.OnStep += (object o, float frac) =>
+            {
+                float pos = (float)positionTween.GetCurrentValue();
+                menu.Visible = pos < 1f;
+                SetMenuPosition(pos);
+            };
+        }
+
+        private void SetMenuPosition(float anchorTop)
+        {
+            menu.AnchorTop = anchorTop;
+            menu.AnchorBottom = anchorTop + 1f;
         }
 
         private PackedScene LoadSettingNodeScene(string metadataName)
