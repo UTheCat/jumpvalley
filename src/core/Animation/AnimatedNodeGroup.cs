cb2b0a2d214f1135700ba457cfa98b80f8ccf1a0
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-05-28 18:15:48 -0700

    Update AnimatedNodeGroup.cs

--- src/core/Animation/AnimatedNodeGroup.cs
+++ src/core/Animation/AnimatedNodeGroup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Jumpvalley.Animation
@@ -17,5 +18,29 @@ namespace Jumpvalley.Animation
         /// The corresponding value is the <see cref="AnimatedNode"/> associated with the ID. 
         /// </summary>
         public Dictionary<string, AnimatedNode> NodeList;
+
+        private int _maxVisibleNodes;
+
+        /// <summary>
+        /// The maximum amount of <see cref="AnimatedNode"/>s within <see cref="NodeList"/>
+        /// that can be shown at a time.
+        /// <br/><br/>
+        /// Set this to -1 to specify that there should be no maximum amount.
+        /// </summary>
+        public int MaxVisibleNodes
+        {
+            get => _maxVisibleNodes;
+            set
+            {
+                if (value < -1) throw new ArgumentOutOfRangeException("", "Maximum amount must be set to an integer greater than or equal to -1");
+                _maxVisibleNodes = value;
+            }
+        }
+
+        public AnimatedNodeGroup()
+        {
+            NodeList = new Dictionary<string, AnimatedNode>();
+            MaxVisibleNodes = -1;
+        }
     }
 }
