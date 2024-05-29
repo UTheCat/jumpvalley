bbd688fb0fec4a6aab9b8b5fc60d6effe8e2dea8
UTheDev <83146800+UTheDev@users.noreply.github.com>
2024-05-29 13:24:20 -0700

    Update BgPanelAnimatedNodeGroup.cs

--- src/core/Animation/BgPanelAnimatedNodeGroup.cs
+++ src/core/Animation/BgPanelAnimatedNodeGroup.cs
@@ -1,3 +1,4 @@
+using System;
 using Jumpvalley.Gui;
 
 namespace Jumpvalley.Animation
@@ -6,7 +7,7 @@ namespace Jumpvalley.Animation
     /// Version of <see cref="AnimatedNodeGroup"/> that displays a background panel
     /// when at least one of the <see cref="AnimatedNode"/>s in the group are visible. 
     /// </summary>
-    public partial class BgPanelAnimatedNodeGroup : AnimatedNodeGroup
+    public partial class BgPanelAnimatedNodeGroup : AnimatedNodeGroup, IDisposable
     {
         /// <summary>
         /// The group's <see cref="BackgroundPanel"/> that will act as
@@ -17,6 +18,27 @@ namespace Jumpvalley.Animation
         public BgPanelAnimatedNodeGroup(BackgroundPanel bgPanel)
         {
             BgPanel = bgPanel;
+            VisibleNodesUpdated += HandleVisibleNodesUpdated;
+        }
+
+        private void HandleVisibleNodesUpdated(object _o, EventArgs _e)
+        {
+            bool shouldBeVisible = VisibleNodes.Count > 0;
+
+            if (shouldBeVisible != BgPanel.IsVisible)
+            {
+                BgPanel.IsVisible = shouldBeVisible;
+            }
+        }
+
+        /// <summary>
+        /// Disposes of this <see cref="BgPanelAnimatedNodeGroup"/>,
+        /// disconnecting the handler function used to modify the visibilty
+        /// of <see cref="BgPanel"/>
+        /// </summary>
+        public void Dispose()
+        {
+            VisibleNodesUpdated -= HandleVisibleNodesUpdated;
         }
     }
 }
