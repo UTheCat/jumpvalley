329b16d037595e7fbb59b073bbc289c17b0fa2a3
UTheCat <bees9633@gmail.com>
2024-06-07 16:00:10 -0700

    have FullscreenControl derive from SettingBase

---
+++ src/game/Settings/Display/FullscreenControl.cs
@@ -0,0 +1,31 @@
+using Godot;
+
+namespace JumpvalleyGame.Settings.Display
+{
+    public partial class FullscreenControl : SettingBase
+    {
+        public FullscreenControl()
+        {
+            Id = "fullscreen";
+            LocalizationId = "SETTINGS_FULLSCREEN";
+            ActionMapKey = "fullscreen_toggle";
+        }
+
+        public override void Update(object newValue)
+        {
+            base.Update(newValue);
+
+            if (newValue is bool fullscreenEnabled)
+            {
+                if (fullscreenEnabled)
+                {
+                    DisplayServer.WindowSetMode(DisplayServer.WindowMode.Fullscreen);
+                }
+                else
+                {
+                    DisplayServer.WindowSetMode(DisplayServer.WindowMode.Windowed);
+                }
+            }
+        }
+    }
+}
