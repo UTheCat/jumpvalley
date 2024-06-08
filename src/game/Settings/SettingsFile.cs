d8514f3e0b9b591fa0007d95d657ef36c6269961
UTheCat <bees9633@gmail.com>
2024-06-07 23:02:01 -0700

    Create SettingsFile.cs

---
+++ src/game/Settings/SettingsFile.cs
@@ -0,0 +1,12 @@
+namespace JumpvalleyGame.Settings
+{
+    /// <summary>
+    /// Class responsible for loading and saving to <c>settings.json</c>,
+    /// the file on the user's filesystem where the user's settings configuration
+    /// is saved (as a JSON file).
+    /// </summary>
+    public partial class SettingsFile
+    {
+
+    }
+}
