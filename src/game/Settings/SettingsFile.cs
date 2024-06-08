2ddbf3e99d670c68bb906776180955188fb634d3
UTheCat <bees9633@gmail.com>
2024-06-07 23:28:14 -0700

    Update SettingsFile.cs

--- src/game/Settings/SettingsFile.cs
+++ src/game/Settings/SettingsFile.cs
@@ -1,3 +1,7 @@
+using System.Text.Json.Nodes;
+using Godot;
+using Jumpvalley.Logging;
+
 namespace JumpvalleyGame.Settings
 {
     /// <summary>
@@ -7,6 +11,36 @@ namespace JumpvalleyGame.Settings
     /// </summary>
     public partial class SettingsFile
     {
+        public static readonly string FILE_NAME = "settings.json";
 
+        private ConsoleLogger logger;
+        private FileAccess file;
+
+        public string SettingsFileLocation;
+
+        public JsonNode Data;
+
+        public SettingsFile()
+        {
+            logger = new ConsoleLogger(nameof(SettingsFile));
+
+            SettingsFileLocation = $"user://{FILE_NAME}";
+        }
+
+        public void Read()
+        {
+            logger.Print("Now attempting to read settings file");
+
+            string fileLocation = SettingsFileLocation;
+
+            if (FileAccess.FileExists(fileLocation))
+            {
+                file = FileAccess.Open(fileLocation, FileAccess.ModeFlags.Read);
+            }
+            else
+            {
+                logger.Print($"{fileLocation} doesn't exist. A new settings file at this location should be created when Write() is called");
+            }
+        }
     }
 }
