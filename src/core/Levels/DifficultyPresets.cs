9c69fc960671307efa440ee1504149aa320a8d62
UTheCat <bees9633@gmail.com>
2024-06-10 23:39:29 -0700

    add the colors for the difficulties

--- src/core/Levels/DifficultyPresets.cs
+++ src/core/Levels/DifficultyPresets.cs
@@ -24,13 +24,13 @@ namespace Jumpvalley.Levels
             new List<Difficulty>()
             {
                 new Difficulty("Beginner", 0.0, new Color(231f / 255f, 0.41f, 1f)),
-                new Difficulty("Intermediate", 15.0, new Color(231f / 255f, 0.41f, 1f)),
-                new Difficulty("Advanced", 30.0, new Color(231f / 255f, 0.41f, 1f)),
-                new Difficulty("Expert", 45.0, new Color(231f / 255f, 0.41f, 1f)),
-                new Difficulty("Grandmaster", 60.0, new Color(231f / 255f, 0.41f, 1f)),
-                new Difficulty("GM+1", 80.0, new Color(231f / 255f, 0.41f, 1f)),
-                new Difficulty("GM+2", 90.0, new Color(231f / 255f, 0.41f, 1f)),
-                new Difficulty("GM+3", 95.0, new Color(231f / 255f, 0.41f, 1f)),
+                new Difficulty("Intermediate", 15.0, new Color(198f / 255f, 0.44f, 1f)),
+                new Difficulty("Advanced", 30.0, new Color(140f / 255f, 0.47f, 0.88f)),
+                new Difficulty("Expert", 45.0, new Color(95f / 255f, 0.53f, 1f)),
+                new Difficulty("Grandmaster", 60.0, new Color(51f / 255f, 0.47f, 1f)),
+                new Difficulty("GM+1", 80.0, new Color(36f / 255f, 0.55f, 1f)),
+                new Difficulty("GM+2", 90.0, new Color(8f / 255f, 0.6f, 1f)),
+                new Difficulty("GM+3", 95.0, new Color(332f / 255f, 0.92f, 1f)),
             }
         );
     }
