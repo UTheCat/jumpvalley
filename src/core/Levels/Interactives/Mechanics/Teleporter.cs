239f970eee6010898646fb86aee13d3f4134741c
UTheCat <bees9633@gmail.com>
2024-06-19 16:20:59 -0700

    Update Teleporter.cs

--- src/core/Levels/Interactives/Mechanics/Teleporter.cs
+++ src/core/Levels/Interactives/Mechanics/Teleporter.cs
@@ -4,10 +4,22 @@ using Jumpvalley.Timing;
 namespace Jumpvalley.Levels.Interactives
 {
     /// <summary>
-    /// Interactive that sends a Node3D to its destination.
+    /// Interactive that sends a Node3D to a defined destination.
     /// </summary>
     public partial class Teleporter : InteractiveNode
     {
+        /// <summary>
+        /// Whether or not the Node3D being teleported should have
+        /// its rotation set to the rotation of <see cref="Destination"/> 
+        /// </summary>
+        public bool ShouldSetRotation;
+
+        /// <summary>
+        /// Where <see cref="Node3D"/>s being teleported by this teleporter
+        /// should be sent
+        /// </summary>
+        public Node3D Destination;
+
         public Teleporter(OffsetStopwatch stopwatch, Node actualNode) : base(stopwatch, actualNode) { }
     }
 }
