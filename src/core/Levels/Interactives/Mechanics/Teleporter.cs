074974e4f2f6bdb655caf6cb92c336cebdb23b4a
UTheCat <bees9633@gmail.com>
2024-06-26 18:19:56 -0700

    Update Teleporter.cs

--- src/core/Levels/Interactives/Mechanics/Teleporter.cs
+++ src/core/Levels/Interactives/Mechanics/Teleporter.cs
@@ -18,12 +18,26 @@ namespace Jumpvalley.Levels.Interactives
         /// Where <see cref="Node3D"/>s being teleported by this teleporter
         /// should be sent.
         /// <br/><br/>
-        /// If this node is a <see cref="MeshInstance3D"/> and has a metadata entry named <c>teleports_on_top</c> set to true,
+        /// If this node is a <see cref="VisualInstance3D"/> and has a metadata entry named <c>teleports_on_top</c> set to true,
         /// objects sent to this destination will be teleported on top of this node.
         /// </summary>
         public Node3D Destination;
 
         public Teleporter(OffsetStopwatch stopwatch, Node marker) : base(stopwatch, marker) { }
+
+        /// <summary>
+        /// Gets the 3D coordinates of the actual point
+        /// where nodes will get sent to when being teleported
+        /// by this teleporter.
+        /// <br/><br/>
+        /// If <see cref="Destination"/> is a <see cref="VisualInstance3D"/> and has a metadata entry named <c>teleports_on_top</c> set to true,
+        /// objects sent to this destination will be teleported on top of this node.
+        /// </summary>
+        /// <returns></returns>
+        public Vector3 GetDestinationPoint()
+        {
+            return Vector3.Zero;
+        }
 
         /// <summary>
         /// Sends a node to the destination specified in <see cref="Destination"/> 
