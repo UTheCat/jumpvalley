0472c39745b7eeaa59fc2beefc609dd4ba59483b
UTheCat <bees9633@gmail.com>
2024-06-26 18:09:36 -0700

    Update Teleporter.cs

--- src/core/Levels/Interactives/Mechanics/Teleporter.cs
+++ src/core/Levels/Interactives/Mechanics/Teleporter.cs
@@ -16,10 +16,22 @@ namespace Jumpvalley.Levels.Interactives
 
         /// <summary>
         /// Where <see cref="Node3D"/>s being teleported by this teleporter
-        /// should be sent
+        /// should be sent.
+        /// <br/><br/>
+        /// If this node is a <see cref="MeshInstance3D"/> and has a metadata entry named <c>teleports_on_top</c> set to true,
+        /// objects sent to this destination will be teleported on top of this node.
         /// </summary>
         public Node3D Destination;
 
         public Teleporter(OffsetStopwatch stopwatch, Node marker) : base(stopwatch, marker) { }
+
+        /// <summary>
+        /// Sends a node to the destination specified in <see cref="Destination"/> 
+        /// </summary>
+        /// <param name="node">The node to send to the destination</param>
+        public void SendToDestination(Node3D node)
+        {
+
+        }
     }
 }
