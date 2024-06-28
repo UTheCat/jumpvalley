026b1b282e2f31f7dd3ce9b7cbe1206282e2ebe5
UTheCat <bees9633@gmail.com>
2024-06-27 19:32:01 -0700

    Update TeleporterTest.cs

--- src/game/Testing/TeleporterTest.cs
+++ src/game/Testing/TeleporterTest.cs
@@ -9,7 +9,7 @@ namespace JumpvalleyGame.Testing
     /// <summary>
     /// Class that tests the functionality of the <see cref="Teleporter"/> class 
     /// </summary>
-    public partial class TeleporterTest
+    public partial class TeleporterTest : IDisposable
     {
         private Node3D nodeToTeleport;
         private Node3D destination;
@@ -29,6 +29,11 @@ namespace JumpvalleyGame.Testing
         public void Teleport()
         {
             teleporter.SendToDestination(nodeToTeleport);
+        }
+
+        public void Dispose()
+        {
+            teleporter.Dispose();
         }
     }
 }
