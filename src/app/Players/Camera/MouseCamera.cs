421382d03e01c996623e496c55ba8558f659d226
UTheCat <bees9633@gmail.com>
2024-11-27 19:38:06 -0800

    add new properties to MouseCamera

--- src/app/Players/Camera/MouseCamera.cs
+++ src/app/Players/Camera/MouseCamera.cs
@@ -14,9 +14,17 @@ namespace JumpvalleyApp.Players.Camera
         private static readonly string INPUT_CAMERA_ZOOM_IN = "camera_zoom_in";
         private static readonly string INPUT_CAMERA_ZOOM_OUT = "camera_zoom_out";
 
-        private bool isTurningCamera = false;
-        //private bool isZoomingCamera = false;
-        //private Vector2 lastMousePos = Vector2.Zero;
+        private bool _isTurningCamera = false;
+
+        public bool IsTurningCamera
+        {
+            get => _isTurningCamera;
+            private set
+            {
+                bool valueChanged = value != _isTurningCamera;
+                _isTurningCamera = value;
+            }
+        }
 
         /// <summary>
         /// The amount of meters that the zoom out distance changes for every frame that the camera is being zoomed in or out by user input.
@@ -40,12 +48,12 @@ namespace JumpvalleyApp.Players.Camera
                     }
                 }
                 */
-                isTurningCamera = true;
+                IsTurningCamera = true;
                 Input.MouseMode = Input.MouseModeEnum.Captured;
             }
             else if (Input.IsActionJustReleased(INPUT_CAMERA_PAN))
             {
-                isTurningCamera = false;
+                IsTurningCamera = false;
                 Input.MouseMode = Input.MouseModeEnum.Visible;
             }
             else if (Input.IsActionPressed(INPUT_CAMERA_ZOOM_IN))
@@ -58,7 +66,7 @@ namespace JumpvalleyApp.Players.Camera
             }
 
             // Right-click to turn the camera
-            if (isTurningCamera && @event is InputEventMouseMotion mouseEvent)
+            if (IsTurningCamera && @event is InputEventMouseMotion mouseEvent)
             {
                 Vector2 mouseEventRelative = mouseEvent.Relative;
 
@@ -80,9 +88,20 @@ namespace JumpvalleyApp.Players.Camera
         public new void Dispose()
         {
             SetProcessInput(false);
-            isTurningCamera = false;
+            IsTurningCamera = false;
 
             base.Dispose();
         }
+
+        /// <summary>
+        /// Event raised when <see cref="IsTurningCamera"/> changes.
+        /// The boolean event argument is the new value of <see cref="IsTurningCamera"/>.
+        /// <br/><br/>
+        /// Avoid accessing the <see cref="IsTurningCamera"/> property directly
+        /// in an event handler method handling this event. This is because the
+        /// <see cref="MouseCamera"/> might've been disposed by the time
+        /// the event handler method has been called.
+        /// </summary>
+        public event EventHandler<bool> IsTurningCameraToggled;
     }
 }
