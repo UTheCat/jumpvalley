0075c22e9727c41de5ae33f2c7e00b8a937f7731
UTheCat <bees9633@gmail.com>
2025-02-06 14:07:08 -0800

    Replace occurrences of JToH with EToH
    Juke's Towers of Hell was recently renamed to Eternal Towers of Hell.

--- src/core/Music/MusicZonePlayer.cs
+++ src/core/Music/MusicZonePlayer.cs
@@ -7,7 +7,7 @@ namespace Jumpvalley.Music
     /// <summary>
     /// MusicPlayer that handles the playback of <see cref="Playlist"/>s with music zones in mind.
     /// <br/>
-    /// Music zone logic is handled like how it is in Juke's Towers of Hell.
+    /// Music zone logic is handled like how it is in Eternal Towers of Hell.
     /// This means that whenever a player enters a music zone, the zone's playlist gets played.
     /// <br/>
     /// In order for music to play when the player is outside a music zone, a primary playlist must be set.
