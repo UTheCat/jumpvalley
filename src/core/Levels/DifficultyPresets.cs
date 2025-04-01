39dba20bebc038a688f65394c65283ef0bffda25
UTheCat <bees9633@gmail.com>
2025-03-31 17:39:02 -0700

    use Color.FromHsv

--- src/core/Levels/DifficultyPresets.cs
+++ src/core/Levels/DifficultyPresets.cs
@@ -31,14 +31,14 @@ namespace UTheCat.Jumpvalley.Core.Levels
         public readonly static DifficultySet PRIMARY_DIFFICULTIES = new DifficultySet(
             new List<Difficulty>()
             {
-                new Difficulty("Beginner", 0.0, new Color(90f / 360f, 0.41f, 1f)),
-                new Difficulty("Intermediate", 20.0, new Color(60f / 360f, 0.41f, 1f)),
-                new Difficulty("Advanced", 40.0, new Color(30f / 360f, 0.41f, 1f)),
-                new Difficulty("Expert", 60.0, new Color(0f / 360f, 0.41f, 1f)),
-                new Difficulty("Grandmaster", 80.0, new Color(330f / 360f, 0.41f, 1f)),
-                new Difficulty("GM+1", 100.0, new Color(300f / 360f, 0.41f, 1f)),
-                new Difficulty("GM+2", 120.0, new Color(270f / 360f, 0.41f, 1f)),
-                new Difficulty("GM+3", 140.0, new Color(240f / 360f, 0.41f, 1f))
+                new Difficulty("Beginner", 0.0, Color.FromHsv(90f / 360f, 0.41f, 1f)),
+                new Difficulty("Intermediate", 20.0, Color.FromHsv(60f / 360f, 0.41f, 1f)),
+                new Difficulty("Advanced", 40.0, Color.FromHsv(30f / 360f, 0.41f, 1f)),
+                new Difficulty("Expert", 60.0, Color.FromHsv(0f / 360f, 0.41f, 1f)),
+                new Difficulty("Grandmaster", 80.0, Color.FromHsv(330f / 360f, 0.41f, 1f)),
+                new Difficulty("GM+1", 100.0, Color.FromHsv(300f / 360f, 0.41f, 1f)),
+                new Difficulty("GM+2", 120.0, Color.FromHsv(270f / 360f, 0.41f, 1f)),
+                new Difficulty("GM+3", 140.0, Color.FromHsv(240f / 360f, 0.41f, 1f))            
             }
         );
     }
