2d34759dafae250c81f90a9373f236201a43081a
UTheCat <bees9633@gmail.com>
2025-03-31 17:03:01 -0700

    change difficulty colors

--- src/core/Levels/DifficultyPresets.cs
+++ src/core/Levels/DifficultyPresets.cs
@@ -32,13 +32,13 @@ namespace UTheCat.Jumpvalley.Core.Levels
             new List<Difficulty>()
             {
                 new Difficulty("Beginner", 0.0, new Color(90f / 360f, 0.41f, 1f)),
-                new Difficulty("Intermediate", 20.0, new Color(60f / 360f, 0.44f, 1f)),
-                new Difficulty("Advanced", 40.0, new Color(30f / 360f, 0.47f, 0.88f)),
-                new Difficulty("Expert", 60.0, new Color(0f / 360f, 0.53f, 1f)),
-                new Difficulty("Grandmaster", 80.0, new Color(330f / 360f, 0.47f, 1f)),
-                new Difficulty("GM+1", 100.0, new Color(300f / 360f, 0.55f, 1f)),
-                new Difficulty("GM+2", 120.0, new Color(270f / 360f, 0.6f, 1f)),
-                new Difficulty("GM+3", 140.0, new Color(240f / 360f, 0.92f, 1f)),
+                new Difficulty("Intermediate", 20.0, new Color(60f / 360f, 0.41f, 1f)),
+                new Difficulty("Advanced", 40.0, new Color(30f / 360f, 0.41f, 1f)),
+                new Difficulty("Expert", 60.0, new Color(0f / 360f, 0.41f, 1f)),
+                new Difficulty("Grandmaster", 80.0, new Color(330f / 360f, 0.41f, 1f)),
+                new Difficulty("GM+1", 100.0, new Color(300f / 360f, 0.41f, 1f)),
+                new Difficulty("GM+2", 120.0, new Color(270f / 360f, 0.41f, 1f)),
+                new Difficulty("GM+3", 140.0, new Color(240f / 360f, 0.41f, 1f))
             }
         );
     }
