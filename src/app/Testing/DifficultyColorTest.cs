e5238951f5e73e38ab9b731353695a9f6fd96abc
UTheCat <bees9633@gmail.com>
2025-03-31 17:26:03 -0700

    implement DifficultyColorTest

--- src/app/Testing/DifficultyColorTest.cs
+++ src/app/Testing/DifficultyColorTest.cs
@@ -9,16 +9,32 @@ namespace JumpvalleyApp.Testing
     /// </summary>
     public partial class DifficultyColorTest : System.IDisposable
     {
-        private Panel panel;
+        private readonly string TEST_LEVEL_NAME = "A Mess of Parts";
+        private readonly string TEST_LEVEL_AUTHOR = "DifficultyColorTester";
+
+        public GridContainer UIGrid { get; private set; }
 
         public DifficultyColorTest()
         {
-            
+            UIGrid = new GridContainer();
+
+            foreach (Difficulty d in DifficultyPresets.PRIMARY_DIFFICULTIES.Difficulties)
+            {
+                Label label = new Label();
+                label.Text = $"{TEST_LEVEL_NAME} by {TEST_LEVEL_AUTHOR} [{d.Name} - {d.Rating}]";
+
+                LabelSettings labelSettings = new LabelSettings();
+                labelSettings.FontColor = d.Color;
+
+                label.LabelSettings = labelSettings;
+                UIGrid.AddChild(label);
+            }
         }
 
         public void Dispose()
         {
-            panel.Dispose();
+            UIGrid.QueueFree();
+            UIGrid.Dispose();
         }
     }
 }
