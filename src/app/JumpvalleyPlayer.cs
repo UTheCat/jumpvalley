85de9cafa11e969efdd2937df217a8c9453246e8
UTheCat <bees9633@gmail.com>
2025-06-22 12:15:23 -0700

    increase acceleration and decceleration of character in JumpvalleyPlayer
    Jolt physics seems to handle acceleration differently. Values pertaining to character acceleration specified in JumpvalleyPlayer were increased to account for this, giving the player more control over their character's movement.

--- src/app/JumpvalleyPlayer.cs
+++ src/app/JumpvalleyPlayer.cs
@@ -119,10 +119,10 @@ namespace UTheCat.Jumpvalley.App
             Mover.Gravity = PhysicsServer3D.AreaGetParam(RootNode.GetViewport().FindWorld3D().Space, PhysicsServer3D.AreaParameter.Gravity).As<float>();
             Mover.JumpVelocity = 25f;
             Mover.Speed = 10f;
-            Mover.Acceleration = 180f;
-            Mover.AirAcceleration = 90f;
-            Mover.Deceleration = 180f;
-            Mover.AirDeceleration = 90f;
+            Mover.Acceleration = 300f;
+            Mover.AirAcceleration = 200f;
+            Mover.Deceleration = 300f;
+            Mover.AirDeceleration = 200f;
             Mover.Rotator.Speed = 12f;
 
             Mover.Body = Character;
