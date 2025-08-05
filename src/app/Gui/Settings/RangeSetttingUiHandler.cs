d0ee982b0215de9082305465168c69d6e4961a02
UTheCat <bees9633@gmail.com>
2025-08-05 01:04:17 -0700

    implement RangeSettingUiHandler

--- src/app/Gui/Settings/RangeSetttingUiHandler.cs
+++ src/app/Gui/Settings/RangeSetttingUiHandler.cs
@@ -17,21 +17,32 @@ namespace UTheCat.Jumpvalley.App.Gui.Settings
             this.setting = setting;
             slider = rangeSettingUi.GetNode<Slider>("Slider");
             lineEdit = rangeSettingUi.GetNode<LineEdit>("LineEdit");
+
+            lineEdit.PlaceholderText = $"{slider.MinValue}-{slider.MaxValue}";
+
+            slider.ValueChanged += OnSliderValueChanged;
+            lineEdit.TextChanged += OnLineEditValueChanged;
         }
 
         public void Dispose()
         {
-
+            slider.ValueChanged -= OnSliderValueChanged;
+            lineEdit.TextChanged -= OnLineEditValueChanged;
         }
 
         private void OnSliderValueChanged(double val)
         {
-
+            lineEdit.Text = val.ToString();
+            setting.Value = val;
         }
 
-        private void OnLineEditValueChanged(string val)
+        private void OnLineEditValueChanged(string sVal)
         {
-            
+            double newVal;
+            if (double.TryParse(sVal, out newVal))
+            {
+                slider.Value = newVal;
+            }
         }
     }
 }
