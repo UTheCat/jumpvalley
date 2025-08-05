8f1781ef618e152244b3075287d15668983d6926
UTheCat <bees9633@gmail.com>
2025-08-05 01:23:41 -0700

    add CameraSensitivitySetting.cs

---
+++ src/app/Settings/Gameplay/CameraSensitivitySetting.cs
@@ -0,0 +1,25 @@
+using UTheCat.Jumpvalley.Core.Players.Camera;
+
+namespace UTheCat.Jumpvalley.App.Settings.Gameplay
+{
+    public partial class CameraSensitivitySetting : RangeSetting
+    {
+        private BaseCamera camera;
+
+        public CameraSensitivitySetting(BaseCamera camera)
+        {
+            Value = false;
+            Id = "camera_sensitivity";
+            LocalizationId = "SETTINGS_CAMERA_SENSITIVITY";
+            this.camera = camera;
+        }
+
+        public override void Update(object newValue)
+        {
+            // Type-checking is performed in RangeSetting.Update()
+            base.Update(newValue);
+
+            camera.PanningSensitivity = (float)newValue;
+        }
+    }
+}
