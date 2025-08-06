6af951daef9298baf1fc035d3ceeec11953541b4
UTheCat <bees9633@gmail.com>
2025-08-06 01:32:19 -0700

    indicate when invalid input is entered into range setting text field

--- src/app/Gui/Settings/RangeSetttingUiHandler.cs
+++ src/app/Gui/Settings/RangeSetttingUiHandler.cs
@@ -8,15 +8,21 @@ namespace UTheCat.Jumpvalley.App.Gui.Settings
     /// </summary>
     public partial class RangeSettingUiHandler : SettingUiHandler, IDisposable
     {
+        private static readonly Color LINE_EDIT_ERR_COLOR = Color.Color8(255, 124, 115);
         private RangeSetting setting;
         private Slider slider;
         private LineEdit lineEdit;
+        private StyleBoxFlat lineEditFocusStyle;
+        private Color lineEditFocusStyleOriginalBorderColor;
 
         public RangeSettingUiHandler(RangeSetting setting, Node rangeSettingUi) : base(setting, rangeSettingUi)
         {
             this.setting = setting;
             slider = rangeSettingUi.GetNode<Slider>("Slider");
             lineEdit = rangeSettingUi.GetNode<LineEdit>("LineEdit");
+            lineEditFocusStyle = lineEdit.GetThemeStylebox("focus") as StyleBoxFlat;
+            if (lineEditFocusStyle == null) throw new Exception("StyleBox of RangeSetting LineEdit node has to be a StyleBoxFlat");
+            lineEditFocusStyleOriginalBorderColor = lineEditFocusStyle.BorderColor;
 
             setting.RangeInstance.Share(slider);
 
@@ -41,13 +47,27 @@ namespace UTheCat.Jumpvalley.App.Gui.Settings
         private void OnLineEditValueChanged(string sVal)
         {
             double newVal;
-            if (double.TryParse(sVal, out newVal))
+
+            // Because of floating-point precision errors, this conditional will have problems when range restriction is requested
+            // and min or max have lots of decimals.
+            // 
+            // But when min and max are integers (as is the case for now), this should generally be okay.
+            if (
+                double.TryParse(sVal, out newVal)
+                && (slider.AllowLesser || !(newVal < slider.MinValue))
+                && (slider.AllowGreater || !(newVal > slider.MaxValue))
+            )
             {
-                if (!slider.AllowLesser) newVal = Math.Max(slider.MinValue, newVal);
-                if (!slider.AllowGreater) newVal = Math.Min(slider.MaxValue, newVal);
+                lineEditFocusStyle.BorderColor = lineEditFocusStyleOriginalBorderColor;
+
                 slider.SetValueNoSignal(newVal);
 
                 setting.Value = newVal;
+            }
+            else
+            {
+                // Let the user know that input is invalid
+                lineEditFocusStyle.BorderColor = LINE_EDIT_ERR_COLOR;
             }
         }
     }
