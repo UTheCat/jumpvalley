b0be93500b1b336065fe76c0e4bd61ce71bb87de
UTheCat <bees9633@gmail.com>
2025-08-21 21:16:39 -0700

    change when fast-turn indicator appears

--- src/app/Gui/FastTurnIndicator.cs
+++ src/app/Gui/FastTurnIndicator.cs
@@ -1,29 +1,30 @@
 using Godot;
 using System;
-
-using UTheCat.Jumpvalley.Core.Players.Movement;
+using UTheCat.Jumpvalley.App.Players.Movement;
 
 namespace UTheCat.Jumpvalley.App.Gui
 {
     /// <summary>
-    /// Indicator that's shown when the user has fast-turn enabled
-    /// and hidden otherwise.
+    /// Indicator that's shown when the user <i>explicitly</i> has fast-turn enabled and hidden otherwise.
+    /// <br/><br/>
+    /// An example of the user explicitly toggling fast-turn is when the user presses a button to enable or disable fast turn.
+    /// By contrast, in the context of this class, entering/exiting first-person doesn't count as explicitly toggling fast-turn.
     /// </summary>
     public partial class FastTurnIndicator : IDisposable
     {
-        private BaseMover mover;
+        private FastTurnControl fastTurnControl;
         private Control actualControl;
 
-        public FastTurnIndicator(BaseMover initMover, Control initActualControl)
+        public FastTurnIndicator(FastTurnControl initFastTurnControl, Control initActualControl)
         {
-            if (initMover == null) throw new ArgumentNullException(nameof(initMover), $"Attempted to pass a {nameof(BaseMover)} that doesn't exist.");
+            if (initFastTurnControl == null) throw new ArgumentNullException(nameof(initFastTurnControl), $"Attempted to pass a {nameof(FastTurnControl)} that doesn't exist.");
             if (initActualControl == null) throw new ArgumentNullException(nameof(initActualControl), $"Attempted to pass a {nameof(Control)} that doesn't exist.");
 
-            mover = initMover;
+            fastTurnControl = initFastTurnControl;
             actualControl = initActualControl;
 
-            ToggleIndicatorVisibility(mover.IsFastTurnEnabled);
-            mover.OnFastTurnToggled += HandleFastTurnToggled;
+            ToggleIndicatorVisibility(initFastTurnControl.UserEnabledFastTurn);
+            fastTurnControl.UserToggledFastTurn += HandleUserToggledFastTurn;
         }
 
         private void ToggleIndicatorVisibility(bool isVisible)
@@ -31,14 +32,14 @@ namespace UTheCat.Jumpvalley.App.Gui
             actualControl.Visible = isVisible;
         }
 
-        private void HandleFastTurnToggled(object _o, bool enabled)
+        private void HandleUserToggledFastTurn(object _o, bool enabled)
         {
             ToggleIndicatorVisibility(enabled);
         }
 
         public void Dispose()
         {
-            mover.OnFastTurnToggled -= HandleFastTurnToggled;
+            fastTurnControl.UserToggledFastTurn -= HandleUserToggledFastTurn;
         }
     }
 }
