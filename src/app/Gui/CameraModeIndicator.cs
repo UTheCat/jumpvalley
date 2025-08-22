9551e535947ca738812d4cffc463f201a607d623
UTheCat <bees9633@gmail.com>
2025-08-21 20:16:42 -0700

    implement first person decal

--- src/app/Gui/CameraModeIndicator.cs
+++ src/app/Gui/CameraModeIndicator.cs
@@ -6,14 +6,18 @@ using UTheCat.Jumpvalley.App.Players.Camera;
 namespace UTheCat.Jumpvalley.App.Gui
 {
     /// <summary>
-    /// Indicator that's shown when the player's camera's rotation
-    /// is responding to user input
-    /// and hidden otherwise.
+    /// Indicator that's shown when the player's camera's rotation is responding to user input and hidden otherwise.
+    /// <br/><br/>
+    /// Indicator appearance changes when switching between first-person and third-person.
     /// </summary>
     public partial class CameraModeIndicator : IDisposable
     {
         private UserInputCamera userInputCamera;
         private Control actualControl;
+        private TextureRect cameraModeTexture;
+
+        private CompressedTexture2D cameraTurnDecal;
+        private CompressedTexture2D cameraTurnFirstPersonDecal;
 
         public CameraModeIndicator(UserInputCamera camera, Control initActualControl)
         {
@@ -22,9 +26,13 @@ namespace UTheCat.Jumpvalley.App.Gui
 
             userInputCamera = camera;
             actualControl = initActualControl;
+            cameraModeTexture = initActualControl.GetNode<TextureRect>("ModeTexture");
+            cameraTurnDecal = ResourceLoader.Load<CompressedTexture2D>("res://decals/camera_turn.svg");
+            cameraTurnFirstPersonDecal = ResourceLoader.Load<CompressedTexture2D>("res://decals/camera_turn_first_person.svg");
 
             ToggleIndicatorVisibility(userInputCamera.IsTurningCamera);
             userInputCamera.IsTurningCameraToggled += HandleIsTurningCameraToggled;
+            userInputCamera.ZoomOutDistanceChanged += HandleZoomOutDistanceChanged;
         }
 
         private void ToggleIndicatorVisibility(bool isVisible)
@@ -37,9 +45,18 @@ namespace UTheCat.Jumpvalley.App.Gui
             ToggleIndicatorVisibility(enabled);
         }
 
+        private void HandleZoomOutDistanceChanged(object _o, float zoomOutDist)
+        {
+            cameraModeTexture.Texture = (zoomOutDist <= 0f) ? cameraTurnFirstPersonDecal : cameraTurnDecal;
+        }
+
         public void Dispose()
         {
             userInputCamera.IsTurningCameraToggled -= HandleIsTurningCameraToggled;
+            userInputCamera.ZoomOutDistanceChanged -= HandleZoomOutDistanceChanged;
+
+            cameraTurnDecal.Dispose();
+            cameraTurnFirstPersonDecal.Dispose();
         }
     }
 }
