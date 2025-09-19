5690e7be29b043d06d9db69c081e9e11d80d28e3
UTheCat <bees9633@gmail.com>
2025-09-18 19:45:52 -0700

    Update AnimatedNode documentation

--- src/core/Animation/AnimatedNode.cs
+++ src/core/Animation/AnimatedNode.cs
@@ -18,7 +18,14 @@ namespace UTheCat.Jumpvalley.Core.Animation
 
         /// <summary>
         /// Whether or not <see cref="ActualNode"/> should be visible.
-        /// Toggling this property will run the animation for showing/hiding <see cref="ActualNode"/>.
+        /// Classes deriving from <see cref="AnimatedNode"/> will typically display show/hide animations when this property is toggled. 
+        /// <br/><br/>
+        /// This property does not indicate whether or not <see cref="ActualNode"/> is actually visible. For example,
+        /// <see cref="IsVisible"/> could be set to false while <see cref="ActualNode"/> is still visible
+        /// (but an animation is playing to hide <see cref="ActualNode"/>).
+        /// <br/><br/>
+        /// To determine whether or not <see cref="ActualNode"/> is actually visible, use <see cref="ActualNode"/>'s Visible property
+        /// (or a similar equivalent if <see cref="ActualNode"/> isn't a <see cref="Node2D"/>, <see cref="Node3D"/>, or <see cref="Control"/>). 
         /// </summary>
         public virtual bool IsVisible
         {
@@ -31,7 +38,7 @@ namespace UTheCat.Jumpvalley.Core.Animation
         }
 
         /// <summary>
-        /// Creates a new instance of AnimatedControl
+        /// Creates a new instance of <see cref="AnimatedNode"/> 
         /// </summary>
         /// <param name="actualNode">The root node of the Godot control to be animated</param>
         public AnimatedNode(Node actualNode)
@@ -42,8 +49,13 @@ namespace UTheCat.Jumpvalley.Core.Animation
         }
 
         /// <summary>
-        /// Event that's raised when <see cref="IsVisible"/> gets toggled.
-        /// The boolean argument of this event is the new value of <see cref="IsVisible"/>
+        /// Event that's raised when the <see cref="IsVisible"/> is set or changed.
+        /// The boolean argument of this event is the new value of <see cref="IsVisible"/>.
+        /// <br/><br/>
+        /// This event will fire even if something sets the <see cref="IsVisible"/> property to the same value
+        /// (meaning the truth value of <see cref="IsVisible"/> didn't change after setting the property.)
+        /// Store the "old/previous" value of <see cref="IsVisible"/> somewhere to check if the value of
+        /// <see cref="IsVisible"/> actually changed. 
         /// </summary>
         public event EventHandler<bool> VisibilityChanged;
 
@@ -57,6 +69,9 @@ namespace UTheCat.Jumpvalley.Core.Animation
         /// <br/><br/>
         /// To assist with garbage collection, this method will automatically call
         /// the <see cref="AnimatedNode"/>'s QueueFree method.
+        /// <br/><br/>
+        /// By default, the implementation of this Dispose() method does nothing to <see cref="ActualNode"/>
+        /// (e.g. the default implementation of this Dispose() method doesn't free <see cref="ActualNode"/> from memory).
         /// </summary>
         public new void Dispose() => QueueFree();
     }
